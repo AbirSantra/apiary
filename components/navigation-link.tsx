340b33ee7e4e84a55d7f8607f88cc4b1d9482ba5
Abir Santra <santraabir8@gmail.com>
2023-08-31 22:03:56 +0530

    removed unused import

--- components/navigation-link.tsx
+++ components/navigation-link.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import React, { ReactElement } from "react";
+import React from "react";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
