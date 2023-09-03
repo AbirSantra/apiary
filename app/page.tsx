d969ef73ca899f54565c8d80bc72376aa7bf112c
Abir Santra <santraabir8@gmail.com>
2023-09-03 19:47:35 +0530

    added homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 export default function Home() {
   return (
-    // <div className="padding-x grid h-[calc(100dvh-4rem)] grid-cols-[auto_20rem] grid-rows-1 gap-4 py-4">
-    //   {/* Main Section */}
-    //   <section className="bg-white">
-    //     <div>Header</div>
-    //     <div>New feed</div>
-    //   </section>
-    //   {/* Side Section */}
-    //   <section className="bg-white">
-    //     <div>Weather</div>
-    //     <div>Currency</div>
-    //   </section>
-    // </div>
-    <div>Test</div>
+    <div className="padding-x padding-y grid h-[calc(100dvh-4rem)] grid-cols-1 grid-rows-1 gap-4 md:grid-cols-[auto_20rem]">
+      {/* Main Section */}
+      <section className="bg-white">
+        <div>Header</div>
+        <div>New feed</div>
+      </section>
+      {/* Side Section */}
+      <section className="hidden bg-white md:flex">
+        <div>Weather</div>
+        <div>Currency</div>
+      </section>
+    </div>
   );
 }
