92843ab051ca9f88aa18fde8ab3eca763cf43212
Abir Santra <santraabir8@gmail.com>
2023-09-12 21:17:39 +0530

    added geolocation to weather section

--- components/weather-section.tsx
+++ components/weather-section.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import useGeolocation from "@/hooks/useGeolocation";
+import useWeather from "@/hooks/useWeather";
 import { MapPin } from "lucide-react";
 import React from "react";
 
 const WeatherSection = () => {
+  const { lat, lon, error } = useGeolocation();
+  console.log(`Lat: ${lat}, Lon: ${lon}, Error: ${error}`);
+
+  // const { weatherData, error: weatherError } = useWeather({
+  //   lat: lat,
+  //   lon: lon,
+  // });
+
+  // console.log(weatherData);
+  // console.log(weatherError);
+
   return (
     <div className="flex w-full flex-col gap-4 rounded-lg bg-white p-6 dark:bg-slate-800">
       <p className="text-sm font-semibold uppercase text-indigo-600">
