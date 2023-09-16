f8f3dd70e9e552e33a6e1d9b7d2e9aa2f8cb2b14
Abir Santra <santraabir8@gmail.com>
2023-09-16 21:21:56 +0530

    refactored weather result interface

--- types/weather-types.ts
+++ types/weather-types.ts
@@ -1,28 +1,49 @@
-export interface CurrentWeatherType {
-  dt: number;
-  sunrise: number;
-  sunset: number;
-  temp: number;
-  feels_like: number;
-  pressure: number;
-  humidity: number;
-  uvi: number;
-  wind_speed: number;
-}
-
-export interface WeatherType {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
-
 export interface WeatherResultType {
-  lat: number;
-  lon: number;
-  timezone: string;
-  current: CurrentWeatherType;
-  weather: WeatherType[];
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  rain: {
+    "1h": number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
 }
 
 export interface WeatherErrorType {
