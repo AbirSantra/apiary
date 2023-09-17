c4e8f30de62ed0379b5444f360873b4c38b9c1ed
Abir Santra <santraabir8@gmail.com>
2023-09-17 19:34:24 +0530

    created icon map

---
+++ constants/weather-icon-map.tsx
@@ -0,0 +1,41 @@
+import oneDay from "../public/weather-icons/01d.png";
+import oneNight from "../public/weather-icons/01n.png";
+import twoDay from "../public/weather-icons/02d.png";
+import twoNight from "../public/weather-icons/02n.png";
+import threeDay from "../public/weather-icons/03d.png";
+import threeNight from "../public/weather-icons/03n.png";
+import fourDay from "../public/weather-icons/04d.png";
+import fourNight from "../public/weather-icons/04n.png";
+import nineDay from "../public/weather-icons/09d.png";
+import nineNight from "../public/weather-icons/09n.png";
+import tenDay from "../public/weather-icons/10d.png";
+import tenNight from "../public/weather-icons/10n.png";
+import elevenDay from "../public/weather-icons/11d.png";
+import elevenNight from "../public/weather-icons/11n.png";
+import thirteenDay from "../public/weather-icons/13d.png";
+import thirteenNight from "../public/weather-icons/13n.png";
+import fiftyDay from "../public/weather-icons/50d.png";
+import fiftyNight from "../public/weather-icons/50n.png";
+
+const weatherIconMap = {
+  "01d": oneDay,
+  "01n": oneNight,
+  "02d": twoDay,
+  "02n": twoNight,
+  "03d": threeDay,
+  "03n": threeNight,
+  "04d": fourDay,
+  "04n": fourNight,
+  "09d": nineDay,
+  "09n": nineNight,
+  "10d": tenDay,
+  "10n": tenNight,
+  "11d": elevenDay,
+  "11n": elevenNight,
+  "13d": thirteenDay,
+  "13n": thirteenNight,
+  "50d": fiftyDay,
+  "50n": fiftyNight,
+};
+
+export default weatherIconMap;
