e15a37e20031441f3b243c600fb5dc94033be2fc
Abir Santra <santraabir8@gmail.com>
2023-09-18 21:01:10 +0530

    added getCoords button

--- hooks/useGeolocation.ts
+++ hooks/useGeolocation.ts
@@ -12,19 +12,12 @@ const useGeolocation = () => {
   const [locationError, setLocationError] = React.useState<String | null>(null);
 
   React.useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLatitude(position.coords.latitude);
-          setLongitude(position.coords.longitude);
-        },
-        (error) => {
-          setLatitude(22.57);
-          setLongitude(88.37);
-          setLocationError(
-            "Geolocation not available. Showing results for default location: Kolkata, India",
-          );
-        },
+    if (localStorage.getItem("coords")) {
+      setLatitude(
+        JSON.parse(localStorage.getItem("coords") as string).latitude,
+      );
+      setLongitude(
+        JSON.parse(localStorage.getItem("coords") as string).longitude,
       );
     } else {
       setLatitude(22.57);
@@ -35,7 +28,32 @@ const useGeolocation = () => {
     }
   }, []);
 
-  return { latitude, locationError, longitude };
+  const getCoords = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+          localStorage.setItem(
+            "coords",
+            JSON.stringify({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            }),
+          );
+        },
+        (error) => {
+          setLatitude(22.57);
+          setLongitude(88.37);
+          setLocationError(
+            "Geolocation not available. Showing results for default location: Kolkata, India",
+          );
+        },
+      );
+    }
+  };
+
+  return { latitude, locationError, longitude, getCoords };
 };
 
 export default useGeolocation;
