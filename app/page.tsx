5bf3ae0529483819494842e8d3c31a5f5b1eba3e
Abir Santra <santraabir8@gmail.com>
2023-09-18 21:01:41 +0530

    changed page width

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import WeatherSection from "@/components/weather-section";
 
 export default function Home() {
   return (
-    <div className="padding-x padding-y relative grid min-h-[calc(100dvh-4rem)] grid-cols-1 grid-rows-1 gap-4 md:grid-cols-[auto_20rem]">
+    <div className="padding-x padding-y relative mx-auto grid min-h-[calc(100dvh-4rem)] w-full max-w-[1440px] grid-cols-1 grid-rows-1 gap-4 md:grid-cols-[auto_20rem]">
       {/* Main Section */}
       <section className="flex flex-col gap-4">
         <NewsSection />
