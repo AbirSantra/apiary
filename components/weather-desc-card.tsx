356937fe01f932c7a7dd86215859dc60e1c4d63a
Abir Santra <santraabir8@gmail.com>
2023-09-22 16:15:20 +0530

    removed currency

---
+++ components/weather-desc-card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WeatherDescCard = () => {
+  return (
+    <div className="flex h-full flex-col gap-2 rounded-lg bg-white p-6">
+      <p className="text-sm font-semibold uppercase text-indigo-500">
+        Today&apos;s Highlights
+      </p>
+    </div>
+  );
+};
+
+export default WeatherDescCard;
