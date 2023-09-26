cc4f8ea7f7a4a1dc17b2202d6bb4e9e41eb8c50c
Abir Santra <santraabir8@gmail.com>
2023-09-26 20:07:57 +0530

    added api call to get daily data

--- hooks/useWeather.ts
+++ hooks/useWeather.ts
@@ -1,22 +1,30 @@
 import React from "react";
 import axios from "axios";
-import { WeatherErrorType, WeatherResultType } from "@/types/weather-types";
+import {
+  DailyWeatherType,
+  WeatherErrorType,
+  WeatherResultType,
+} from "@/types/weather-types";
 
 const API_KEY = process.env.NEXT_PUBLIC_OPENWEATHER_APPID;
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
+const DAILY_URL = `https://api.openweathermap.org/data/2.5/onecall`;
 
 const useWeather = ({ city }: { city: string | null }) => {
   const [weatherData, setWeatherData] =
     React.useState<WeatherResultType | null>(null);
+  const [dailyWeather, setDailyWeather] = React.useState<DailyWeatherType[]>(
+    [],
+  );
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState<WeatherErrorType | null>(null);
 
   React.useEffect(() => {
     const fetchWeather = async () => {
-      console.log("Fetching Data");
       setLoading(true);
 
       try {
+        console.log("Fetching Data");
         const response = await axios(BASE_URL, {
           params: {
             q: city,
@@ -25,7 +33,22 @@ const useWeather = ({ city }: { city: string | null }) => {
           },
         });
 
+        const lat = response.data.coord.lat;
+        const lon = response.data.coord.lon;
+
+        const dailyData = await axios(DAILY_URL, {
+          params: {
+            lat: lat,
+            lon: lon,
+            appid: API_KEY,
+            units: "metric",
+            cnt: 7,
+            exclude: "current,minutely,hourly,alerts",
+          },
+        });
+
         setWeatherData(response.data);
+        setDailyWeather(dailyData.data.daily);
         setLoading(false);
       } catch (error: any) {
         setError(error);
@@ -38,7 +61,7 @@ const useWeather = ({ city }: { city: string | null }) => {
     }
   }, [city]);
 
-  return { weatherData, loading, error };
+  return { weatherData, loading, error, dailyWeather };
 };
 
 export default useWeather;
