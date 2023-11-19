27e173441db2745c2c3d5041c0b005999f0a36fb
Abir Santra <santraabir8@gmail.com>
2023-11-19 11:40:50 +0530

    removed forecast description

--- components/weather-week-card.tsx
+++ components/weather-week-card.tsx
@@ -12,7 +12,7 @@ const WeatherWeekCard = ({
   loading: boolean;
 }) => {
   return (
-    <div className="flex h-full flex-col gap-6 rounded-lg bg-white p-6 dark:bg-slate-800">
+    <div className="flex h-full flex-col gap-4 rounded-lg bg-white p-6 dark:bg-slate-800">
       <p className="text-sm font-semibold uppercase text-indigo-500">
         7-day Forecast
       </p>
@@ -37,13 +37,12 @@ const WeatherDayCard = ({ data }: { data: DailyWeatherType }) => {
   const icon =
     weatherIconMap[data?.weather[0].icon as keyof typeof weatherIconMap];
   return (
-    <div className="flex h-full items-center justify-between py-4">
-      <p className="font-medium uppercase text-slate-400">
+    <div className="flex h-full items-center justify-between py-2">
+      <p className="text-sm font-medium uppercase text-slate-400">
         {moment.unix(data.dt).format("ddd")}
       </p>
       <div className="flex items-center justify-center gap-2">
         <Image src={icon} alt={"weather-icon"} height={48} width={48} />
-        <p className="text-sm text-slate-400">{data.weather[0].main}</p>
       </div>
       <p className="font-medium text-slate-500">
         <span className="font-semibold text-indigo-500">
@@ -61,7 +60,6 @@ const WeatherDayCardSkeleton = () => {
       <p className="h-4 w-12 rounded-md bg-slate-200 font-medium uppercase text-slate-400"></p>
       <div className="flex items-center justify-center gap-2">
         <div className="h-12 w-12 rounded-md bg-slate-200" />
-        <p className="h-3 w-12 rounded-md bg-slate-200 text-sm text-slate-400"></p>
       </div>
       <p className="h-4 w-16 rounded-md bg-slate-200 font-medium text-slate-500"></p>
     </div>
